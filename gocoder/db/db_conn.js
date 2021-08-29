07fc201022868494c7c6fdadd637c3154bdc3d42
Jeyoung-Park <59302192+Jeyoung-Park@users.noreply.github.com>
2021-08-30 00:06:42 +0900

    fix: db 폴더 module.exports 참조 오류 수정
    -db 폴더 module.exports 참조 오류 수정

--- gocoder/db/db_conn.js
+++ gocoder/db/db_conn.js
@@ -1,15 +1,17 @@
 var mysql=require('mysql');
-var config=require('./db_info').local;
+var config=require('./db_info');
 
-modules.exports=function(){
+// console.log('config in db_conn', config);
+
+module.exports=function(){
     return{
         init:function(){
             return mysql.createConnection({
-                host:config.host,
-                port:config.port,
-                user:config.user,
-                password:config.password,
-                database:config.database,
+                host:config.getDBInfo().local.host,
+                port:config.getDBInfo().local.port,
+                user:config.getDBInfo().local.user,
+                password:config.getDBInfo().local.password,
+                database:config.getDBInfo().local.database,
             })
         }
     }
